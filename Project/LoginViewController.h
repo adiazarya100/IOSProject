52edfaa96d1d394eff6bc9ff1ab2d4c3d3caf77e
tomer aronovsky <tomeraronovsky@tomers-macbook-pro.local>
2015-12-23 18:52:14 +0200

    Changes in Signup & Login screens

---
+++ Project/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  Project
+//
+//  Created by tomer aronovsky on 12/23/15.
+//  Copyright © 2015 Adi Azarya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+- (IBAction)SignInButton:(id)sender;
+
+@end
